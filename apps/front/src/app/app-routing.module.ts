342dd0a4d2af83b2004c1f30b6552fc78684282b
Raul-Duarte <raulduarte.info@gmail.com>
2021-08-14 10:42:54 -0300

    commit-inicial

---
+++ apps/front/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [
+  {
+    path: 'tarefas',
+    loadChildren: () =>
+      import('./modules/task/task.module').then((m) => m.TaskModule),
+  },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
