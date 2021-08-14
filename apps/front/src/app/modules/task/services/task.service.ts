f4d288007ef71b217d1f76a68314f7bbd4514c70
Raul-Duarte <raulduarte.info@gmail.com>
2021-08-14 11:40:36 -0300

    initial

---
+++ apps/front/src/app/modules/task/services/task.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ITask } from '@nt-al/api-interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Promise<ITask[] | undefined> {
+    return this.http.get<ITask[]>('/api/tasks').toPromise();
+  }
+}
--- apps/front/src/app/modules/task/task.component.ts
+++ apps/front/src/app/modules/task/task.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from './services/task.service';
+import { ITask } from '@nt-al/api-interfaces';
 
 @Component({
   selector: 'nt-al-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.scss']
+  styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent implements OnInit {
-
-  constructor() { }
+  public tasks: ITask[] | undefined;
+  constructor(public service: TaskService) {}
 
   ngOnInit(): void {
+    this.getTasks();
   }
 
+  getTasks(): void {
+    this.service.getAll().then((tasks) => {
+      this.tasks = tasks;
+      console.log(this.tasks);
+    });
+  }
 }
