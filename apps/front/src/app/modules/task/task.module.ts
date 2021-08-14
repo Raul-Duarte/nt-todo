1b748e76e95230dbe340470ff54619450243aab0
Raul-Duarte <raulduarte.info@gmail.com>
2021-08-14 14:35:51 -0300

    feat: inserindo material angular

--- apps/front/src/app/modules/task/task.module.ts
+++ apps/front/src/app/modules/task/task.module.ts
@@ -1,17 +1,21 @@
+// noinspection AngularInvalidImportedOrDeclaredSymbol
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TaskRoutingModule } from './task-routing.module';
 import { TaskComponent } from './task.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
   declarations: [
-    TaskComponent
+    TaskComponent,
   ],
   imports: [
     CommonModule,
-    TaskRoutingModule
+    TaskRoutingModule,
+    MatCardModule
   ]
 })
 export class TaskModule { }
